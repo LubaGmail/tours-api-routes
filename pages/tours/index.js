604285c24ee3eef61e3223cdd1bb1b31e5054bb9
LL Powe <powe2009@gmail.com>
2022-10-25 16:56:33 -0400

    getStaticProps

--- pages/tours/index.js
+++ pages/tours/index.js
@@ -2,21 +2,19 @@ import { useState, useEffect } from 'react'
 import { useRouter } from 'next/router'
 import ToursData, {getTours} from '../../components/util/tours-data'
 
-import { getAllTours } from '../../data/dummy-tours'
+// import { getAllTours } from '../../data/dummy-tours'
 import ToursList from '../../components/tours/tours-list'
 import SearchForm from '../../components/tours/search-form'
 
 const AllToursPage = props => {
     const [tours, setTours] = useState(props.tours)
     const router = useRouter()
-    // console.log('p', props.tours)
 
     // useEffect(() => {
-    //     setTours(getAllTours())
+    //     setTours(getTours())
     // }, [])
 
     const handleSearch = (year, month) => {
-        console.log('allToursPage.handleSearch', year, month)
         const fullPath = `/tours/${year}/${month}`
         router.push(fullPath);
     }
@@ -32,12 +30,12 @@ const AllToursPage = props => {
 
 export async function getStaticProps(context) {
     const tours = await getTours()
-    // console.log('t', tours)
 
     return {
         props: {
             tours: tours
-        }
+        },
+        revalidate: 1000
     }
 }
 
