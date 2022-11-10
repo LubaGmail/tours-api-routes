96f1682a5df20d7d689904923a3c039727bc3076
LL Powe <powe2009@gmail.com>
2022-11-09 20:57:46 -0500

    cosmetic

--- components/tours/search-form.js
+++ components/tours/search-form.js
@@ -30,40 +30,40 @@ const SearchForm = ({handleSearch}) => {
 
     return (
 
-        <div className={styles.container}>
+        <div>
             <form onSubmit={handleSubmit} className={styles.form}>
 
-                    <div className={styles.item}>
-                        <label htmlFor='year'>Year: </label>
-                        <select id='year' name='year' ref={yearRef}>
-                            <option value='2022'>2022</option>
-                            <option value='2023'>2023</option>
-                        </select>
-                    </div>
+                <div>
+                    <label htmlFor='year'>Year: </label>
+                    <select id='year' name='year' ref={yearRef}>
+                        <option value='2022'>2022</option>
+                        <option value='2023'>2023</option>
+                    </select>
+                </div>
 
-                    <div className={styles.item}>
-                        <label htmlFor='month'>Month: </label>
-                        <select id='month' name='month' ref={monthRef}>
-                            <option value='1'>January</option>
-                            <option value='2'>February</option>
-                            <option value='3'>March</option>
-                            <option value='4'>April</option>
-                            <option value='5'>May</option>
-                            <option value='6'>June</option>
-                            <option value='7'>July</option>
-                            <option value='8'>August</option>
-                            <option value='9'>September</option>
-                            <option value='10'>October</option>
-                            <option value='11'>November</option>
-                            <option value='12'>December</option>
-                        </select>
-                    </div>
-                        
-                    <div className={styles.item}>
-                        <button type='submit' id='btn' name='btn'>
-                            Search
-                        </button>
-                    </div>
+                <div>
+                    <label htmlFor='month'>Month: </label>
+                    <select id='month' name='month' ref={monthRef}>
+                        <option value='1'>January</option>
+                        <option value='2'>February</option>
+                        <option value='3'>March</option>
+                        <option value='4'>April</option>
+                        <option value='5'>May</option>
+                        <option value='6'>June</option>
+                        <option value='7'>July</option>
+                        <option value='8'>August</option>
+                        <option value='9'>September</option>
+                        <option value='10'>October</option>
+                        <option value='11'>November</option>
+                        <option value='12'>December</option>
+                    </select>
+                </div>
+                <div>
+                    <label>&nbsp;</label>
+                    <button type='submit' id='btn' name='btn'>
+                        Search
+                    </button>
+                </div>
   
             </form>
         </div>
